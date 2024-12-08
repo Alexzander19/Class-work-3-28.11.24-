7935ba4f16179211f127d81e60794422d353c94e
Alexzander19 <2961781@mail.ru>
2024-12-08 22:26:01 +0700

    Добавлена модель с классом адреса изображения

--- autosale/japancar/admin.py
+++ autosale/japancar/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Auto_new
+
 # Register your models here.
+
+admin.site.register(Auto_new)
--- autosale/japancar/models.py
+++ autosale/japancar/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Auto_new(models.Model):
+    mark = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    we_are_by = models.IntegerField()
+    price = models.IntegerField()
+    picture = models.ImageField(upload_to='img/', height_field=None, width_field=None, max_length=100)
+
+    def __str__(self):
+        return f'{self.mark}: {self.model}'
+
