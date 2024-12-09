f4757b77dfcb5f2337a3a1cfec324c1a20686c2e
Alexzander19 <2961781@mail.ru>
2024-12-09 12:33:41 +0700

    Картинки отображаются, но
    Почему-то изменения файла style.css никак не отображаются на страничке. Плюс картинки работают не из того каталога, что нужно, по-другому не получается их загрузить. Вообще частая у меня проблема с адресацией, что-то недопонимаю

--- autosale/japancar/views.py
+++ autosale/japancar/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect, render
 
+from .models import Auto_new
+
 # Create your views here.
 
 def index(request):
@@ -7,3 +9,8 @@ def index(request):
 
 def about(request):
     return render(request,'showroom/about.html')
+
+def list_of_cars(request):
+
+    context = {'cars': Auto_new.objects.all()}
+    return render(request,'showroom/list_of_cars.html',context=context)
